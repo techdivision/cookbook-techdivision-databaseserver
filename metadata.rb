2050a98a1a323387e825aac3d2f5eaf643326c91
Robert Lemke <robert@typo3.org>
2014-02-20 16:03:31 +0100

    [TASK] Initial commit

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             "techdivision-databaseserver"
+maintainer       "Robert Lemke"
+maintainer_email "r.lemke@techdivision.com"
+license          "MIT License"
+description      "Installs/Configures techdivision-databaseserver"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "0.1.0"
+
+depends "techdivision-base"
+depends "mysql"
+depends "database"
