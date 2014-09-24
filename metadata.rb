ff37d6e6bea00ec9b3f24dc76490b53080a48b30
Robert Lemke <rl@robertlemke.com>
2014-09-24 16:00:24 +0200

    [BUGFIX] Fixes "mysql is not a class" error

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,4 @@ description      "Installs/Configures techdivision-databaseserver"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.1.3"
 
-depends "mysql" "5.3.6"
 depends "database"
